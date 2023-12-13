7feb93395a9642cd1ce2a0a9a892afb264a0a306
karlk <karl-klostermann@t-online.de>
2023-12-13 10:18:02 +0100

    order_history.php updated

--- new_files/templates/bootstrap4/source/boxes/order_history.php
+++ new_files/templates/bootstrap4/source/boxes/order_history.php
@@ -52,7 +52,7 @@ if (!$box_smarty->is_cached(CURRENT_TEMPLATE.'/boxes/box_order_history.html', $c
       while ($orders = xtc_db_fetch_array($orders_query)) {
         $customer_orders_array[] = array(
           'PRODUCTS_LINK' => '<a class="w-100" href="' . xtc_href_link(FILENAME_PRODUCT_INFO, xtc_product_link($orders['products_id'], $orders['products_name'])) . '" style="white-space:normal;">' . xtc_image_button('templates/' . CURRENT_TEMPLATE . '/img/order_history' , $orders['products_name']) . '</a>',
-          'ORDER_LINK' => '<a href="' . xtc_href_link(basename($PHP_SELF), xtc_get_all_get_params(array('action')) . 'action=add_order_product&order_id='.$orders['orders_id'].'&id='.$orders['orders_products_id']) . '">' . xtc_image_button('templates/' . CURRENT_TEMPLATE . '/img/icon_cart.png' , ICON_CART) . '</a>',
+          'ORDER_LINK' => '<a href="' . xtc_href_link(basename($PHP_SELF), xtc_get_all_get_params(array('action')) . 'action=add_order_product&order_id='.$orders['orders_id'].'&id='.$orders['orders_products_id']) . '">' . xtc_image_button('templates/' . CURRENT_TEMPLATE . '/img/icon_cart.gif' , ICON_CART) . '</a>',
         );
       }
     }
